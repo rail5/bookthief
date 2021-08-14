048c8b0653eb4c2f80db5e1d3189dcb14276f2f0
rail5 <3323429-rail5@users.noreply.gitlab.com>
2021-08-14 09:23:06 +0000

    Update BookThief 2.0

---
+++ bookthiefApp.cpp
@@ -0,0 +1,33 @@
+/***************************************************************
+ * Name:      bookthiefApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    rail5 (andrew@rail5.org)
+ * Created:   2021-08-04
+ * Copyright: rail5 (https://rail5.org)
+ * License:
+ **************************************************************/
+
+#include "bookthiefApp.h"
+
+//(*AppHeaders
+#include "bookthiefMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(bookthiefApp);
+
+bool bookthiefApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxInitAllImageHandlers();
+    if ( wxsOK )
+    {
+    	bookthiefFrame* Frame = new bookthiefFrame(0);
+    	Frame->Show();
+    	SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+
+}
