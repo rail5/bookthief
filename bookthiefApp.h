9dcfce0ca3e339661bf1f71b034b0e46b3dc4abf
rail5 <3323429-rail5@users.noreply.gitlab.com>
2021-10-11 21:47:36 +0000

    Update bookthiefApp.cpp, bookthiefApp.h, bookthiefMain.cpp, bookthiefMain.h, makefile, bookthief-screenshot2.png, bookthief-screenshot1.png, debian/control, debian/changelog Deleted pdf-16x16-cc0.xpm, bookthief-screenshot1.png, bookthief-screenshot2.png

--- bookthiefApp.h
+++ bookthiefApp.h
@@ -2,13 +2,13 @@
  * Name:      bookthiefApp.h
  * Purpose:   Defines Application Class
  * Author:    rail5 (andrew@rail5.org)
- * Created:   2021-08-04
+ * Created:   2021-10-10
  * Copyright: rail5 (https://rail5.org)
- * License:   GNU GPL v3.0
+ * License:
  **************************************************************/
 
-#ifndef BOOKTHIEFAPP_H
-#define BOOKTHIEFAPP_H
+#ifndef bookthiefAPP_H
+#define bookthiefAPP_H
 
 #include <wx/app.h>
 
@@ -18,4 +18,4 @@ class bookthiefApp : public wxApp
         virtual bool OnInit();
 };
 
-#endif // BOOKTHIEFAPP_H
+#endif // bookthiefAPP_H
